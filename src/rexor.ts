6919d782f9544d1f24ecd39e576c295e5e1a5f89
Szymon Sirocki <szymon.sirocki@gmail.com>
2020-10-14 20:59:55 +0200

    - Reimplemented bot for 2nd ecition

--- src/rexor.ts
+++ src/rexor.ts
@@ -3,24 +3,20 @@ import { logger } from './logger.js';
 import { Fire } from './firestore.js';
 import { User } from './user';
 import { Slack } from './slack.js'
+import { Secrets } from './secrets.js'
+import { Task } from './task.js';
+import { Timestamp } from '@google-cloud/firestore';
 
 export class Rexor extends Bot {
 
     private static responsesPolish: Map<string, string> = new Map<string, string>([
         ['lang_switch', "No to po polsku!"],
-        ['help', "Wysyłaj mi odpowiedzi na zagadki, które pojawiają się na #general. Osoby, które najszybciej rozwiążą wszystkie zagadki, wygrają specjalne nagrody. \n\n- Odpisz `status`, żeby dowiedzieć się, jak dobrze Ci idzie.\n- Odpisz `rezygnuje`, żeby zrezygnować z konkursu i usunąć wszstkie swoje dane.\n- For English - reply `english please`. :flag-gb:"],
+        ['help', "Czy nasz osoby które znajdują się na obrazku?. Wyślij wszystkie osoby do mnie w jednej wiadomości używając ich @ np. %s. Spośród osób, które odgadną wszytskie osoby prawidłowo, wylosujemy specjalne nagrody. \n\n- Odpisz `status`, żeby dowiedzieć się, jak dobrze Ci idzie.\n- Odpisz `rezygnuje`, żeby zrezygnować z konkursu i usunąć wszstkie swoje dane.\n- For English - reply `english please`. :flag-gb:"],
         ['bad_response', "Niestety nie brzmi to jak odpowiedź, której szukamy :disappointed:. Nie wiesz, co robić? Możesz zawsze zapytać się mnie o `pomoc`. \n\nFor english - reply `English please`. :flag-gb:"],
-        ['riddle_1', "Brawo! Poniedziałkowa zagadka rozwiązana! Rozwiązano już %d z %d zagadek! :tada:"],
-        ['riddle_1_cpl', "Ta zagadka została już przez Ciebie rozwiązana!"],
-        ['riddle_2', "Brawo! Wtorkowa zagadka rozwiązana! Rozwiązano już %d z %d zagadek! :tada:"],
-        ['riddle_2_cpl', "Ta zagadka została już przez Ciebie rozwiązana!"],
-        ['riddle_3', "Brawo! Zagadka ze Środy rozwiązana! Rozwiązano już %d z %d zagadek! :tada:"],
-        ['riddle_3_cpl', "Ta zagadka została już przez Ciebie rozwiązana!"],
-        ['riddle_4', "Brawo! Zagadka z Czwartku rozwiązana! Rozwiązano już %d z %d zagadek! :tada:"],
-        ['riddle_4_cpl', "Ta zagadka została już przez Ciebie rozwiązana!"],
-        ['riddle_5', "Brawo! Piątkowa zagadka rozwiązana! Rozwiązano już %d z %d zagadek! :tada:"],
-        ['riddle_5_cpl', "Ta zagadka została już przez Ciebie rozwiązana!"],
-        ['status', "Twój obecny postęp to %d na %d zagadek!\n\n %s Poniedziałek\n %s Wtorek\n %s Środa\n %s Czwartek\n %s Piątek"],
+        ['riddle_1_solved', "Brawo! :tada: Udało Ci się rozwiązać zagadkę :tada:! Liczba prób: %d."],
+        ['riddle_1_partial', "No niestety nie udało Ci się odgadnąć wszytskich osób dobrze, jedynie %d z %d! Próbuj dalej."],
+        ['riddle_1_cpl', "Już rozwiązałeś naszą zagadkę próbując zaledwie %d razy! Teraz tylko czekamy na wyniki!"],
+        ['status', "Mimo %d prób, nie udało Ci się rozwiązać jeszcze zagadki!"],
         ['withdrawal', "Rozumiem! Usunąłem dane o Tobie. Szkoda, że rezygnujesz z zabawy. Jeśli zdecydujesz się dołączyć jeszcze raz, wystarczy, że do mnie napiszesz. Niestety odpowiedzi na zagadki będą musiały być wysłane jeszcze raz. :wave:"]
     ]);
 
@@ -28,16 +24,9 @@ export class Rexor extends Bot {
         ['lang_switch', "Alright, let's talk in English then!"],
         ['help', "Send me answers to the puzzles which are posted on #general. The people who will get all of the proper answers the fastest will win special rewards!\n\n- Reply `status` to check on your progress.\n- Reply `withdraw` to resign and delete all your data.\n- Odpisz `polski`, żeby porozmawiać po polsku :flag-pl:"],
         ['bad_response', "Unfortunately it doesn't look like the answer we're looking for :disappointed:. Don't know what to do? You can always ask me for `help`. \n\nOdpisz `polski`, żeby porozmawiać po polsku :flag-pl:"],
-        ['riddle_1', "Congratulations! You've solved the Monday's riddle! So far you've solved %d out of %d ridles! :tada:"],
+        ['riddle_1_solved', "Congratulations! You've solved the Monday's riddle! So far you've solved %d out of %d ridles! :tada:"],
+        ['riddle_1_partial', "No niestety nie udało Ci się odgadnąć wszytskich osób dobrze, jedynie %d z %d! Próbuj dalej."],
         ['riddle_1_cpl', "This riddle has already been solved by you!"],
-        ['riddle_2', "Congratulations! You've solved the Tuesday's riddle! So far you've solved %d out of %d ridles! :tada:"],
-        ['riddle_2_cpl', "This riddle has already been solved by you!"],
-        ['riddle_3', "Congratulations! You've solved the Wednesday's riddle! So far you've solved %d out of %d ridles! :tada:"],
-        ['riddle_3_cpl', "This riddle has already been solved by you!"],
-        ['riddle_4', "Congratulations! You've solved the Thursday's riddle! So far you've solved %d out of %d ridles! :tada:"],
-        ['riddle_4_cpl', "This riddle has already been solved by you!"],
-        ['riddle_5', "Congratulations! You've solved the Friday's riddle! So far you've solved %d out of %d ridles! :tada:"],
-        ['riddle_5_cpl', "This riddle has already been solved by you!"],
         ['status', "Your progress is %d out of %d riddles done!\n\n%s Monday\n%s Tuesday\n%s Wednesday\n%s Thursday\n%s Friday"],
         ['withdrawal', "Understood! I've deleted all of your data! It's a shame that you've resigned. If you'd like to re-join just send me any message. You will need to send answers to all riddles again though! :wave:"]
     ]);
@@ -68,9 +57,7 @@ export class Rexor extends Bot {
         logger.info(`user ${userId} on channel ${channelId} with message ${text}`);
 
         const parsableMessage = Rexor.removeDiacritics(text.toLowerCase());
-
-        const personDataRaw: any = await Fire.getUser(userId);
-        let personData: User = personDataRaw.data;
+        let personData: User = await Fire.getUser(userId);
 
         if (!personData) {
 
@@ -98,9 +85,14 @@ export class Rexor extends Bot {
             return
         }
 
+        const sprintf = require('sprintf-js').sprintf;
+
         const helpKeywords = ['pomoc', 'pomoze', 'pomaga', 'help'];
         if (helpKeywords.some(s => parsableMessage.includes(s))) {
-            this.replyWithMessage(dictionary.get('help'), channelId);
+
+            const exampleUserId = await Secrets.getSecret('example-user-id')
+            const reply = sprintf(dictionary.get('help'), exampleUserId);
+            this.replyWithMessage(reply, channelId);
             return
         }
 
@@ -111,107 +103,71 @@ export class Rexor extends Bot {
             return
         }
 
-        const sprintf = require('sprintf-js').sprintf;
-        let solvedRiddles = Array.from(personData.tasks.values()).filter(p => p.completedTs).length;
+        // Fetch task data
+        const taskId = 'task1';
+        let taskData = personData.tasks.find(t => t.id === taskId);
+        logger.info('task data', taskData)
+        if (!taskData) {
+            logger.info('adding new task data');
+            taskData = new Task(taskId);
+            personData.tasks.push(taskData);
+        }
 
-        // const statusKeywords = ['status', 'progres', 'zadani', 'task', 'info'];
-        // if (statusKeywords.some(s => parsableMessage.includes(s))) {
+        const statusKeywords = ['status', 'progres', 'info'];
+        if (statusKeywords.some(s => parsableMessage.includes(s))) {
 
-        //     const reply = sprintf(dictionary.get('status'), solvedRiddles, 5,
-        //         personData.isTaskCompleted('task1') ? ':heavy_check_mark:' : ':x:',
-        //         personData.isTaskCompleted('task2') ? ':heavy_check_mark:' : ':x:',
-        //         personData.isTaskCompleted('task3') ? ':heavy_check_mark:' : ':x:',
-        //         personData.isTaskCompleted('task4') ? ':heavy_check_mark:' : ':x:',
-        //         personData.isTaskCompleted('task5') ? ':heavy_check_mark:' : ':x:');
+            if (taskData.completedTs) {
+                logger.info('already completed');
+                const reply = sprintf(dictionary.get('riddle_1_cpl'), taskData.attempts);
+                this.replyWithMessage(reply, channelId);
+                return;
+            }
 
-        //     this.replyWithMessage(reply, channelId);
-        //     return
-        // }
+            const statusReply = sprintf(dictionary.get('status'), taskData.attempts);
+            this.replyWithMessage(statusReply, channelId);
+            return;
+        }
 
-        this.replyWithMessage("hau!", channelId);
+        // Check if already solved the riddle
+        if (taskData.completedTs) {
+            logger.info('already completed');
+            const reply = sprintf(dictionary.get('riddle_1_cpl'), taskData.attempts);
+            this.replyWithMessage(reply, channelId);
+            return;
+        }
+        // let solvedRiddles = Array.from(personData.tasks.values()).filter(p => p.completedTs).length;
+
+        const correctAnswersRaw: string = await Secrets.getSecret('edition-2-answer');
+        const correctAnswers = correctAnswersRaw.split(',');
+
+        let correctGuesses = 0;
+        correctAnswers.forEach(answer => {
+            if (new RegExp(`@${answer}`).test(text)) {
+                correctGuesses++;
+            }
+        });
+
+        const userIdInMessage = /<@.+>/.test(text);
+
+        if (correctGuesses === correctAnswers.length) {
+            // tada
+            taskData.attempts++;
+            taskData.completedTs = Timestamp.now();
+            const reply = sprintf(dictionary.get('riddle_1_solved'), taskData.attempts);
+            this.replyWithMessage(reply, channelId);
+            await Fire.upsertData(personData);
+        } else if (correctGuesses === 0 && !userIdInMessage) {
+            // No guesses and no users in message
+            this.replyWithMessage(dictionary.get('bad_response'), channelId);
+        }
+        else {
+            logger.info('something in between')
+            // somethin in between
+            taskData.attempts++;
+            const reply = sprintf(dictionary.get('riddle_1_partial'), correctGuesses, correctAnswers.length, taskData.attempts);
+            this.replyWithMessage(reply, channelId);
+            await Fire.upsertData(personData);
+        }
         return;
-
-        // Monday
-        if (/\b2[., ]?9[., ]?1[., ]?4[., ]?9[., ]?1[., ]?7\b/.test(parsableMessage)) {
-            const taskId: string = "task1";
-
-            if (personData.isTaskCompleted(taskId)) {
-                this.replyWithMessage(dictionary.get('riddle_1_cpl'), channelId);
-            }
-            else {
-                solvedRiddles++;
-                this.replyWithMessage(sprintf(dictionary.get('riddle_1'), solvedRiddles, 5), channelId);
-                personData.completeTask(taskId);
-                await Fire.upsertData(personData);
-            }
-            return;
-        }
-
-        // Tuesday
-        if (/1\.3\.1\b/.test(parsableMessage)) {
-            const taskId: string = "task2";
-
-            if (personData.isTaskCompleted(taskId)) {
-                this.replyWithMessage(dictionary.get('riddle_2_cpl'), channelId);
-            }
-            else {
-                solvedRiddles++;
-                this.replyWithMessage(sprintf(dictionary.get('riddle_2'), solvedRiddles, 5), channelId);
-                personData.completeTask(taskId);
-                await Fire.upsertData(personData);
-            }
-            return;
-        }
-
-        // Wed
-        if (/5984#/.test(parsableMessage)) {
-            const taskId: string = "task3";
-
-            if (personData.isTaskCompleted(taskId)) {
-                this.replyWithMessage(dictionary.get('riddle_3_cpl'), channelId);
-            }
-            else {
-                solvedRiddles++;
-                this.replyWithMessage(sprintf(dictionary.get('riddle_3'), solvedRiddles, 5), channelId);
-                personData.completeTask(taskId);
-                await Fire.upsertData(personData);
-            }
-            return;
-        }
-
-        // Thu
-        if (/\b(0+)?11111100011\b/.test(parsableMessage)) {
-            const taskId: string = "task4";
-
-            if (personData.isTaskCompleted(taskId)) {
-                this.replyWithMessage(dictionary.get('riddle_4_cpl'), channelId);
-            }
-            else {
-                solvedRiddles++;
-                this.replyWithMessage(sprintf(dictionary.get('riddle_4'), solvedRiddles, 5), channelId);
-                personData.completeTask(taskId);
-                await Fire.upsertData(personData);
-            }
-            return;
-        };
-
-        // Fri
-        if (parsableMessage.includes('podejdz prosze')) {
-            const taskId: string = "task5";
-
-            if (personData.isTaskCompleted(taskId)) {
-                this.replyWithMessage(dictionary.get('riddle_5_cpl'), channelId);
-            }
-            else {
-                solvedRiddles++;
-                this.replyWithMessage(sprintf(dictionary.get('riddle_5'), solvedRiddles, 5), channelId);
-                personData.completeTask(taskId);
-                await Fire.upsertData(personData);
-            }
-            return;
-        };
-
-        this.replyWithMessage(dictionary.get('bad_response'), channelId);
-        return
     }
 }
